3e5174062b5565a3f3b058854f0354f7a075ddfb
jungslee@c10r6s5.42seoul.kr <you@example.com>
2023-10-12 15:23:43 +0900

    libft

---
+++ libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jungslee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 14:24:15 by jungslee          #+#    #+#             */
+/*   Updated: 2023/10/11 14:46:20 by jungslee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include	"ft_isalpha.c"
+# include	"ft_isdigit.c"
+# include	"ft_isalnum.c"
+# include	"ft_isascii.c"
+# include	"ft_isprint.c"
+# include	"ft_strlen.c"
+
+# endif
