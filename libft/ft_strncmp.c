b9e953e33cee1799a6731a90eecbe75251e51c0b
jungslee@c10r6s5.42seoul.kr <you@example.com>
2023-10-21 17:28:45 +0900

    part 1 - calloc - strdup

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jungslee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:33:59 by jungslee          #+#    #+#             */
-/*   Updated: 2023/10/17 22:06:07 by jungslee         ###   ########.fr       */
+/*   Updated: 2023/10/21 14:59:17 by jungslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,25 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	num = 0;
-	while (num < n)
+	while (num < n && (*(str1 + num) || *(str2 + num)))
 	{
 		if (*(str1 + num) != *(str2 + num))
 			return (*(str1 + num) - *(str2 + num));
 		num++;
 	}
-	return (*(str1 + num) - *(str2 + num));
+	return (0);
 }
+/*
+#include	<stdio.h>
+#include	<string.h>
+
+int	main(void)
+{
+	char s1[] = "atoms\0\0\0\0";
+	char s2[] = "atomsee\0abc";
+	int	result1 = strncmp(s1, s2, 8);
+	int result2 = ft_strncmp(s1, s2, 8);
+
+	printf("strncmp : %d\nft_strncmp : %d\n", result1, result2);
+}
+*/
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,51 +6,44 @@
 /*   By: jungslee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 20:24:03 by jungslee          #+#    #+#             */
-/*   Updated: 2023/10/17 22:05:35 by jungslee         ###   ########.fr       */
+/*   Updated: 2023/10/21 17:12:38 by jungslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
-static int	find_str(const char *haystack_portion, const char *needle)
-{
-	char	*s_portion;
-	char	*n;
-
-	s_portion = (char *)haystack_portion;
-	n = (char *)needle;
-	while (*n != 0)
-	{
-		if (*s_portion != *n)
-		{
-			return (0);
-		}
-		n++;
-		s_portion++;
-	}
-	return (1);
-}
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char	*str;
-	char	*find;
-	size_t	i;
+	size_t	iter;
+	size_t	needle_len;
 
-	str = (char *)haystack;
-	find = (char *)needle;
-	i = 0;
-	if (*find == '\0')
-		return (str);
-	while (i < len && *str)
+	needle_len = ft_strlen((char *)needle);
+	iter = 0;
+	if (*needle == '\0')
 	{
-		if (*find == *str)
-		{
-			if (find_str(str, find))
-				return (str);
-		}
-		str++;
-		i++;
+		return ((char *)haystack);
 	}
-	return (NULL);
+	while (*haystack && (iter + needle_len <= len))
+	{
+		if (ft_strncmp(haystack + iter, needle, needle_len) == 0)
+		{
+			return ((char *)(haystack + iter));
+		}
+		iter++;
+	}
+	return (0);
 }
+/*
+#include	<string.h>
+#include	<stdio.h>
+
+int	main(void)
+{
+	char	haystack[] = "";
+	char	needle[] = "fake";
+
+	printf("%c", *ft_strnstr(((void *)0), "fake", 3));
+	//ft_strnstr(haystack, needle, 3);
+	//strnstr(NL, needle, 3);
+}
+*/
